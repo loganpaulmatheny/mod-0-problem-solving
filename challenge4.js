0bdf28651ba89f8185d06b05d682b807378d8839
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-27 13:35:17 -0400

    Update comment challenge4.js

--- challenge4.js
+++ challenge4.js
@@ -1,5 +1,10 @@
 // Start with an array of hobbies. Print out only the words that end in "ing".
 
+// create an array of hobbies
+// create a function
+// loop through the array
+// use the 'endsWith()' method with conditional to print hobbies that end with inghttps://github.com/loganpaulmatheny/mod-0-problem-solving
+
 var hobbies = [
   "soccer",
   "drawing",
